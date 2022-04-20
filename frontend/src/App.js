4ffd8f857256c8e2972f762b811dc7bad5fcac35
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2022-04-19 22:04:40 -0700

    adding LinkContainer and Link components from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { Container } from 'react-bootstrap' 
+
 import Header from './components/Header'
 import Footer from './components/Footer'
+
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <main className="py-3">
-        <Container >
-          <HomeScreen/>
-        </Container>
-      </main>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+        <main className="py-3">
+          <Container >
+            <Routes >
+              <Route path='/' element={<HomeScreen/>}/>
+              <Route path='/product/:id' element={<ProductScreen/>}/>
+            </Routes>
+          </Container>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
@@ -7,16 +8,16 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`} >
+            <Link to={`/product/${product._id}`} >
                 <Card.Img src={product.image} variant='top' />
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${product._id}`} >
+                <Link to={`/product/${product._id}`} >
                     <Card.Title as='div'>
-                        <strong></strong>
+                        <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
 
             <Card.Text>
                 <Rating value={product.rating} text={ `${product.numReviews}reviews` } />
